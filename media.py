2ac7ea7d4235d9a0130717531f74d9ef52a81c05
Ytry <Kyle12qw@gmail.com>
2015-10-16 18:00:24 -0400

    documentation
    added docstrings to the class and show_trailer function

--- media.py
+++ media.py
@@ -4,10 +4,17 @@ import webbrowser
 class Movie():
     def __init__(self, movie_title, poster_image,
                  trailer_youtube):
+        """"
+        constructor of the movie class
+        initializes parameters needed in entertainment center class
+        """"
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-
+        """"
+        Test function for class variables
+        opens a new browser tab with a given string as the url
+        """"
         webbrowser.open(self.trailer_youtube_url)
